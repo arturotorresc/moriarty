9b749fa7af431a04a66ff18205e302ab324a39cc
Arturo Torres <arturotc22@gmail.com>
2020-04-13 01:31:04 -0500

    Fixed minor lexer issues, added tests for lexical analyzer.

--- lex.py
+++ lex.py
@@ -1,24 +1,20 @@
 import ply.lex as lex
-import ply.yacc as yacc
- 
-reserved = { 'if': 'IF', 'else': 'ELSE', 'loop': 'LOOP', 'player': 'PLAYER', 'var': 'VAR', 'function': 'FUNCTION',
-             'string': 'STRING', 'list': 'LIST', 'integer': 'INTEGER', 'false': 'FALSE', 'true': 'TRUE',
-             'move': 'MOVE', 'speak': 'SPEAK', 'rotate': 'ROTATE', 'shoot': 'SHOOT', 'jump': 'JUMP',
-             'enemy?': 'ENEMY', 'reload_gun': 'RELOAD', 'gun_loaded?': 'LOADED',
-             'void': 'VOID', 'not': 'NOT', 'return': 'RETURN'}
-tokens = ['ID', 'SIGN', 'OPERATOR', 'RELOP', 'COMMA', 'SEMICOLON',
-            'LPAREN', 'RPAREN', 'LCURL', 'RCURL', 'EQUALS', 'DOTS',
-              'INT', 'BOOL', 'STR'] + list(reserved.values())
 
-t_STR = r'"[a-zA-Z_]*\"'
-t_INT = r'^\d+$'
-t_BOOL = r'^(true|false)'
+reserved = {'if': 'IF', 'else': 'ELSE', 'loop': 'LOOP', 'player': 'PLAYER', 'var': 'VAR', 'function': 'FUNCTION',
+            'string': 'STRING', 'int': 'INT', 'bool': 'BOOL' ,
+            'move': 'MOVE', 'speak': 'SPEAK', 'rotate': 'ROTATE', 'shoot': 'SHOOT', 'jump': 'JUMP',
+            'enemy?': 'ENEMY', 'reload_gun': 'RELOAD_GUN', 'gun_loaded?': 'GUN_LOADED',
+            'void': 'VOID', 'not': 'NOT', 'return': 'RETURN'}
+tokens = ['BOOLEAN', 'SIGN', 'OPERATOR', 'RELOP', 'COMMA', 'SEMICOLON',
+          'LPAREN', 'RPAREN', 'LCURL', 'RCURL', 'EQUALS', 'DOTS',
+          'INTEGER', 'STR', 'ID'] + list(reserved.values())
+
+t_STR = r'".*\"'
 t_SIGN = r'(?:\+|-)'
 t_OPERATOR = r'(?:\*|\/)'
-t_RELOP = r'(?:<|>|<=|>=|!=|and|or)'
 t_COMMA = r'\,'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 t_LCURL = r'\{'
 t_RCURL = r'\}'
 t_EQUALS = r'\='
@@ -26,17 +22,37 @@ t_DOTS = r'\:'
 t_SEMICOLON = r'\;'
 t_ignore = ' \t'
 
-def t_newline(t):
-  r'\n+'
-  t.lexer.lineno += len(t.value)
 
-def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reserved.get(t.value,'ID')
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_BOOLEAN(t):
+    r'true|false'
+    if t.value == 'true':
+        t.value = True
+    else:
+        t.value = False
     return t
 
-def t_error(token):
-  print("Invalid Token:", token.value)
-  token.lexer.skip(1)
+def t_RELOP(t):
+    r'(?:and|or|<=|>=|<|>|!=)'
+    return t
 
-lex.lex()
+def t_INTEGER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*\??'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+
+def t_error(token):
+    print("Invalid Token:", token.value)
+    token.lexer.skip(1)
+
+
+lexer = lex.lex()
