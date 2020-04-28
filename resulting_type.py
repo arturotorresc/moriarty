9bf43153c3154313a84597bd575388de75b7be59
Ana Paola Treviño <anapao.trevino@gmail.com>
2020-04-27 21:45:28 -0500

    Change ResultingType class to static method

--- resulting_type.py
+++ resulting_type.py
@@ -1,67 +1,69 @@
 class ResultingType:
-  def __init__(self):
-    self.ResultingType = {
-      '+': {
-        'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'string' }
-      },
-      '-': {
-        'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '*': {
-        'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '/': {
-        'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '<': {
-        'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '<=': {
-        'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '>': {
-        'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '>=': {
-        'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
-      },
-      '!=': {
-        'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'bool', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'bool' }
-      },
-      '==': {
-        'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
-        'bool':   { 'int': 'ERROR', 'bool': 'bool', 'string': 'ERROR' },
-        'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'bool' }
-      },
-      'and': {
-        'int':    { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
-        'bool':   { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
-        'string': { 'int': 'bool', 'bool': 'bool', 'string': 'bool' }
-      },
-      'or': {
-        'int':    { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
-        'bool':   { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
-        'string': { 'int': 'bool', 'bool': 'bool', 'string': 'bool' }
-      }
+  # Semantic Table for Expressions
+  __cubeType = {
+    '+': {
+      'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'string' }
+    },
+    '-': {
+      'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '*': {
+      'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '/': {
+      'int':    { 'int': 'int', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '<': {
+      'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '<=': {
+      'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '>': {
+      'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '>=': {
+      'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'ERROR' }
+    },
+    '!=': {
+      'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'bool', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'bool' }
+    },
+    '==': {
+      'int':    { 'int': 'bool', 'bool': 'ERROR', 'string': 'ERROR' },
+      'bool':   { 'int': 'ERROR', 'bool': 'bool', 'string': 'ERROR' },
+      'string': { 'int': 'ERROR', 'bool': 'ERROR', 'string': 'bool' }
+    },
+    'and': {
+      'int':    { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
+      'bool':   { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
+      'string': { 'int': 'bool', 'bool': 'bool', 'string': 'bool' }
+    },
+    'or': {
+      'int':    { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
+      'bool':   { 'int': 'bool', 'bool': 'bool', 'string': 'bool' },
+      'string': { 'int': 'bool', 'bool': 'bool', 'string': 'bool' }
     }
-  
+  }
+
+  # Get resulting type between two operators and its operation
+  @classmethod
   def get_type(self, operator, left, right):
-    return ResultingType[operator][left][right]
+    return ResultingType.__cubeType[operator][left][right]
