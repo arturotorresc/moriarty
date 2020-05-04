13359dc0da91b65f53c3430c97006e91e2bed88c
Ana Paola Treviño <anapao.trevino@gmail.com>
2020-05-04 18:03:17 -0500

    Loop intermediate representation

--- yacc.py
+++ yacc.py
@@ -7,7 +7,7 @@ from algorithms import attempt_create_quadruple
 from expression_handler import ExpressionHandler
 from semantic_error import SemanticError
 from quadruple import Quadruple
-from jumps_stack import JumpsStack, PendingJump
+from jumps_stack import JumpsStack, PendingJump, JumpHere
 from quadruple import QuadrupleStack
 
 exp_handler = ExpressionHandler.get_instance()
@@ -185,7 +185,38 @@ def p_assignment_1(p):
                    | EQUALS expression SEMICOLON '''
 
 def p_loop(p):
-  ''' loop : LOOP LPAREN expression RPAREN block '''
+  ''' loop : LOOP add-loop-jump LPAREN expression RPAREN loop-false block loop-end '''
+
+# EMBEDDED ACTIONS
+def p_add_loop_jump(p):
+  ''' add-loop-jump :'''
+  next_quad_id = QuadrupleStack.next_quad_id()
+  jumps_stack = JumpsStack.get_instance()
+  jumps_stack.push_quad(Quadruple(None, None, None, JumpHere(next_quad_id)))
+
+# EMBEDDED ACTIONS
+def p_loop_false(p):
+  ''' loop-false :'''
+  exp_handler = ExpressionHandler.get_instance()
+  var, var_type = exp_handler.pop_operand()
+  if (var_type != 'bool'):
+    raise SemanticError("Result of expression is not of type 'bool'. Found '{}' instead.".format(var_type))
+  jump_quad = Quadruple("GOTOF", var, None, PendingJump())
+  quad_stack = QuadrupleStack.get_instance()
+  quad_stack.push_quad(jump_quad)
+  jumps_stack = JumpsStack.get_instance()
+  jumps_stack.push_quad(jump_quad)
+
+# EMBEDDED ACTIONS
+def p_loop_end(p):
+  ''' loop-end : '''
+  jumps_stack = JumpsStack.get_instance()
+  end = jumps_stack.pop_quad()
+  returning = jumps_stack.pop_quad()
+  quad = Quadruple("GOTO", None, None, returning.result().id)
+  quad_stack = QuadrupleStack.get_instance()
+  quad_stack.push_quad(quad)
+  end.set_jump(QuadrupleStack.next_quad_id())
 
 def p_return(p):
   ''' return : RETURN return-1 '''
