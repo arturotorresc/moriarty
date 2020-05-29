b20b07f63d1eca695a2fa764c82e511ea91c4ff8
Ana Paola Treviño <anapao.trevino@gmail.com>
2020-05-28 21:47:05 -0500

    Fix errors made while merging the branch

--- virtual_machine.py
+++ virtual_machine.py
@@ -4,7 +4,7 @@ from memory import Memory, POINTER_RANGE, LOCAL_RANGE, MEM_SIZE
 from game_state import GameState
 from collections import deque
 
-special_functions = ['move', 'speak', 'rotate', 'shoot', 'jump', 'enemy?', 'reload_gun', 'gun_loaded?']
+special_functions = ['move', 'rotate', 'shoot', 'jump', 'enemy?', 'reload_gun', 'gun_loaded?']
 
 class VirtualMachine:
   def __init__(self, file_name):
@@ -26,7 +26,7 @@ class VirtualMachine:
     next_quad = self.__quadruples[self.__ip]
     while next_quad[0] != 'END_MAIN':
       operator, left, right, result = next_quad
-      # print('OPERATOR====', operator, left, right, result)
+      print('OPERATOR====', operator, left, right, result)
 
       if operator in ['+', '-', '*', '/', '<=', '>=', '<', '>', '!=', '==', 'and', 'or']:
         left_val = memory.get_address_value(left)
@@ -34,18 +34,16 @@ class VirtualMachine:
         value = self.operations(operator, left_val, right_val)
         memory.set_address_value(result, value)
         self.__ip += 1
-      elif operator in ['ABS', 'NEGATIVE']:
+      elif operator in ['ABS', 'NEGATIVE', 'not']:
         left_val = memory.get_address_value(left)
         value = self.flip(operator, left_val)
         memory.set_address_value(result, value)
         self.__ip += 1
       elif operator == '=':
         left_val = memory.get_address_value(left)
+        if result in POINTER_RANGE:
+          result = memory.get_address_value(result, True)
         memory.set_address_value(result, left_val)
-        self.__ip += 1
-      elif operator == 'not':
-        left_val = memory.get_address_value(left)
-        memory.set_address_value(result, not left_val)
         self.__ip += 1
       elif operator == 'GOTO':
         self.__ip = result
@@ -94,17 +92,19 @@ class VirtualMachine:
       elif operator == 'ENDFUNC':
         self.__ip = self.__last_quad.pop()
         memory.pop_locals()
-      elif operator == 'GOTO_MAIN':
-        self.__ip = result
-      elif operator in special_functions:
-        print(operator)
-        self.__ip += 1
       elif operator == 'INIT_GAME':
         GameState.get_instance()
         self.__ip += 1
       elif operator == 'INIT_PLAYER':
         game_state = GameState.get_instance()
         game_state.initialize_player(left)
+        self.__ip += 1
+      elif operator in special_functions:
+        self.special_functions(operator, left)
+        self.__ip += 1
+      elif operator == 'speak':
+        value = memory.get_address_value(left)
+        print(value)
         self.__ip += 1
       # Quitar este else
       else:
@@ -144,4 +144,23 @@ class VirtualMachine:
       return abs(left)
     if operator == 'NEGATIVE':
       return left * (-1)
+    if operator == 'not':
+      return not left
+  
+  def special_functions(self, function, player):
+    game_state = GameState.get_instance()
     
+    if operator == 'move':
+      game_state.move_player(player)
+    elif operator == 'rotate':
+      game_state.rotate_player(player)
+    elif operator == 'shoot':
+      game_state.shoot_enemy(player)
+    elif operator == 'gun_loaded?':
+      game_state.gun_loaded(player)
+    elif operator == 'reload_gun':
+      game_state.reload_gun(player)
+    elif operator == 'enemy?':
+      game_state.enemy_in_front(player)
+    elif operator == 'jump':
+      game_state.jump(player)
